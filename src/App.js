7263c33c0e343a6364d54d650d36eb8e322ded05
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-01-02 16:28:18 +0900

    feat: Create delete all lists

--- src/App.js
+++ src/App.js
@@ -24,11 +24,16 @@ export default function App() {
     setValue("");
   };
 
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 bg-white rounded shadow-md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1 className="text-3xl font-bold underline">할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
         <Lists todoData={todoData} setTodoData={setTodoData} />
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} setTodoData={setTodoData} />
